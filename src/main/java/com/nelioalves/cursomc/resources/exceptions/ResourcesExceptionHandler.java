5aa34b7859a6ad590208e2eae4d764a13aa483ff
Alexandre Pires Ferreira de Miranda <alexandre.miranda@gmail.com>
2019-02-27 16:08:58 -0300

    Estado e cidade

--- src/main/java/com/nelioalves/cursomc/resources/exceptions/ResourcesExceptionHandler.java
+++ src/main/java/com/nelioalves/cursomc/resources/exceptions/ResourcesExceptionHandler.java
@@ -16,8 +16,7 @@ public class ResourcesExceptionHandler {
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
 		
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
-		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);		
 	}
 
 }
