0a481831173a0fbd702554148b3716775927c5a7
Alexandre Pires Ferreira de Miranda <alexandre.miranda@gmail.com>
2019-03-13 19:51:11 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/nelioalves/cursomc/config/DevConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/DevConfig.java
@@ -9,27 +9,34 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.nelioalves.cursomc.services.DBSevice;
+import com.nelioalves.cursomc.services.EmailService;
+import com.nelioalves.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
 public class DevConfig {
-	
+
 	@Autowired
 	private DBSevice dbService;
-	
+
 	@Value("spring.jpa.hibernate.ddl-auto")
 	private String strategy;
-	
+
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
-		
-		if (! "create".equals(strategy)) {
+
+		if (!"create".equals(strategy)) {
 			return false;
 		}
-		
+
 		dbService.instantiateTestDatabase();
-		
+
 		return true;
+	}
+
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 
 }
