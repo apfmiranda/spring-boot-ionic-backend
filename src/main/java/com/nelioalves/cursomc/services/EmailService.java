250e9e503495b6a295f9111be2403be81e8bbd7c
Alexandre Pires Ferreira de Miranda <alexandre.miranda@gmail.com>
2019-03-13 22:45:51 -0300

    Email Html

--- src/main/java/com/nelioalves/cursomc/services/EmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.nelioalves.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
@@ -11,5 +13,9 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 
 }
