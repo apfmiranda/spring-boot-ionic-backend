ba2e50b1ce200f71c544b781200c5a17c8189ec4
Alexandre Pires Ferreira de Miranda <alexandre.miranda@gmail.com>
2019-04-17 13:23:42 -0300

    Acrescentando mais produtos para testar infinity scroll

--- src/main/java/com/nelioalves/cursomc/services/DBSevice.java
+++ src/main/java/com/nelioalves/cursomc/services/DBSevice.java
@@ -2,7 +2,9 @@ package com.nelioalves.cursomc.services;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -80,6 +82,15 @@ public class DBSevice {
 		Produto p10 = new Produto(null, "Pendente", 180.00);
 		Produto p11 = new Produto(null, "Shampoo", 90.00);
 		
+		List<Produto> produtos = new ArrayList<Produto>();
+
+		for (int i = 12 ; i < 51; i++ ) {
+			Produto produto = new Produto(null, "Produto " + i, 10.00);
+			cat1.getProdutos().add(produto);
+			produto.getCategorias().add(cat1);
+			produtos.add(produto);
+		}
+		
 		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2, p4));
 		cat3.getProdutos().addAll(Arrays.asList(p5, p6));
@@ -102,6 +113,8 @@ public class DBSevice {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3,cat4,cat5,cat6,cat7));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
+		
+		produtoRepository.saveAll(produtos);
 
 		Estado est1 = new Estado(null,"Minas Gerais");
 		Estado est2 = new Estado(null,"São Paulo");
